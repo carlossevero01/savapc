a0e51d0a390203affba297b1427d3f0ff664d16e
carlossevero01 <carlos212427@gmail.com>
2022-12-13 16:05:30 -0300

    creation

---
+++ sistematcc/src/main/java/com/ifsul/sistema/computacional/sistematcc/SistematccApplication.java
@@ -0,0 +1,13 @@
+package com.ifsul.sistema.computacional.sistematcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistematccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistematccApplication.class, args);
+	}
+
+}
---
+++ sistematcc/src/test/java/com/ifsul/sistema/computacional/sistematcc/SistematccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsul.sistema.computacional.sistematcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistematccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
